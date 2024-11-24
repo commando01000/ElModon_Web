607bccabbd1aca8194d3c2ad3d8e3aacf124773a
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-11-24 21:21:06 +0200

    modified_structure

---
+++ database/migrations/2024_11_24_184458_create_contact.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->id();
+            // phone number
+            $table->string('phone', 11)->nullable();
+            // email
+            $table->string('email', 100)->nullable();
+            // address
+            $table->string('address', 255)->nullable();
+            // image
+            $table->string('image')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact');
+    }
+};
