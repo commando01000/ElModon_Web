64c32fb038d908a344a79902c347c9b676877ee0
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2024-11-29 23:48:41 +0200

    styles

--- resources/views/Frontend/Home/index.blade.php
+++ resources/views/Frontend/Home/index.blade.php
@@ -1,1 +1,5 @@
-hi
+@include('Frontend.Shared.layout')
+<h1 class="animate__animated animate__bounce">An animated element</h1>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
--- resources/views/Frontend/Shared/layout.blade.php
+++ resources/views/Frontend/Shared/layout.blade.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> @yield('title') | {{ config('app.name') }}</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+     />   <style>
+        @tailwind base;
+        @tailwind components;
+        @tailwind utilities;
+    </style>
+
+     <link href="./css/output.css" rel="stylesheet">
+
+    {{-- <link rel="stylesheet" href="style.css"> --}}
 </head>
 <body>
         <header>
-         @include('layout.nav')
+
         </header>
     <div class="container">
         @yield('content')
