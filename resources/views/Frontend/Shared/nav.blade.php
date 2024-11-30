b4c3d25cc5e8a64fe6857b696fd6024ccc8ddef2
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2024-11-30 15:17:32 +0200

    navbar_responsivity !

--- resources/views/Frontend/Shared/nav.blade.php
+++ resources/views/Frontend/Shared/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary animate__animated animate__fadeInDown">
     <div class="container-fluid">
-        <img src="{{ asset('assets/images/Logo.png') }}" alt="Logo" class="navbar-brand ms-5"/>
+        <img src="{{ asset('assets/images/Logo.png') }}" alt="Logo" class="navbar-brand ms-5" />
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,19 +8,19 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-5 ms-auto">
                 <li class=" nav-item na ">
-                    <a class="nav-link fa fa-home" aria-current="page" href="/home">  Home</a>
+                    <a class="nav-link fa fa-home" aria-current="page" href="/home"> Home</a>
                 </li>
                 <li class="nav-item na">
-                    <a class="nav-link fa fa-bars" href="/projects">  Projects</a>
+                    <a class="nav-link fa fa-bars" href="/projects"> Projects</a>
                 </li>
                 <li class="nav-item na">
-                    <a class="nav-link fa fa-users" href="/Engineers">  Engineers</a>
+                    <a class="nav-link fa fa-users" href="/Engineers"> Engineers</a>
                 </li>
                 <li class="nav-item na">
-                    <a class="nav-link fa fa-book" href="/about-us">  About Us</a>
+                    <a class="nav-link fa fa-book" href="/about-us"> About Us</a>
                 </li>
                 <li class="nav-item na">
-                    <a class="nav-link fa fa-phone" href="/contact-us">  Contact Us</a>
+                    <a class="nav-link fa fa-phone" href="/contact-us"> Contact Us</a>
                 </li>
             </ul>
         </div>
