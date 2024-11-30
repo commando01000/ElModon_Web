b8b6122989c2f7baea17fb6bef072dd8e7d5de7e
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2024-11-30 02:10:49 +0200

    Navbar+home

--- resources/views/Frontend/Home/index.blade.php
+++ resources/views/Frontend/Home/index.blade.php
@@ -1,5 +1,2 @@
 @include('Frontend.Shared.layout')
-<h1 class="animate__animated animate__bounce">An animated element</h1>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+@include('Frontend.Projects-Swiper.swiper')
---
+++ resources/views/Frontend/Shared/nav.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary animate__animated animate__fadeInDown">
+    <div class="container-fluid">
+        <img src="{{ asset('assets/images/Logo.png') }}" alt="Logo" class="navbar-brand ms-5"/>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-5 ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/projects">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about-us">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact-us">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
