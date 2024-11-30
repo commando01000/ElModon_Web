4560084e1d143f28b6ed49ba96c7bd01d5225abc
Youssef Gamal Mohamed <31347791+Youssef-Mekkkawy@users.noreply.github.com>
2024-11-30 03:07:34 +0200

    admin-login-final

--- resources/views/Backend/dashboard/index.blade.php
+++ resources/views/Backend/dashboard/index.blade.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> @yield('title') | {{ config('app.name') }}</title>
+
+    <link href="{{ asset('assets/css/admin_dashboard.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    <h1>Welcome to the Admin Dashboard</h1>
+    <header>
+        @include('Shared.Backend.layout')
+    </header>
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
+
 </html>
--- resources/views/Shared/Backend/layout.blade.php
+++ resources/views/Shared/Backend/layout.blade.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('assets/css/admin_dashboard.css') }}" rel="stylesheets">
     <title>Admin Dashboard</title>
 </head>
+
 <body>
-    <h1>Welcome to the Admin Dashboard</h1>
+    <!-- Main Navigation -->
+    <header>
+
+        @include('Shared.Backend.nav')
+        @include('Shared.Backend.sidebar')
+
+    </header>
+    <!-- Main Navigation -->
+
+    <!--Main layout-->
+    <main style="margin-top: 58px;">
+        <div class="container pt-4"></div>
+    </main>
 </body>
+
 </html>
